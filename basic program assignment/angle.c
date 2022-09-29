a8493c8cf4f6aa178bc4ff2bcb2171b99c8324fb
NIKHIL AGRAWAL <nikhilagrawal0605@gmail.com>
2022-09-29 09:32:01 +0530

    Initial commit

---
+++ basic program assignment/angle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+int b,c,a;
+
+printf("ENTer the value of a and b");
+scanf("%d,%d",&a,&b);
+c=180-a-b;
+printf(" the value of c is %d",c);
+return 0;
+}
---
+++ basic program assignment/lengthconvrsion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+int l;
+printf("enter the value of l");
+scanf("%f")
+
+
+
+
+
+return 0;
+}
