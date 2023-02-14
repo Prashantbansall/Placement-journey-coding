5a0e6542566f997cffd27a046d51c5f935800fb4
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2023-02-15 00:23:53 +0530

    Add files via upload

---
+++ Python Course/15 hours python course bootcamp by code with harry/Chapter 2/02_type_of_variables.py
@@ -0,0 +1,19 @@
+# Printing the type of variables
+a = "prashant"
+b = '''prashant 
+is 
+a 
+good
+boy'''
+c = 367
+d = 49.32
+e = True
+f = False
+g = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
