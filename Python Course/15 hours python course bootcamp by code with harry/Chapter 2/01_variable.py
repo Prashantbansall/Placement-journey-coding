343fe7537807cc42ec54fabc155ca429a1243fb7
Prashant Bansal <114644999+Prashantbansall@users.noreply.github.com>
2023-02-15 00:19:49 +0530

    Create 01_variable.py

---
+++ Python Course/15 hours python course bootcamp by code with harry/Chapter 2/01_variable.py
@@ -0,0 +1,22 @@
+# Printing the variables
+a = "prashant" 
+b = 'prashant' 
+c = '''prashant 
+is 
+a 
+good
+boy'''
+d = 367   
+e = 49.32
+f = True  
+g = False 
+h = None  
+print(a)
+print(b)
+print(e)
+print(d)
+print(c)
+print(f)
+print(g)
+print(h)
+
